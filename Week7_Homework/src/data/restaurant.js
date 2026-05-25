eef0c6fe84205563f582bb8781eff632f0bdc306
LEE SANGBIN <novelsb72@naver.com>
2026-05-25 20:35:01 +0900

    feat: 분식 음식점 데이터 추가

---
+++ Week7_Homework/src/data/restaurant.js
@@ -0,0 +1,63 @@
+export const restaurants = [
+  {
+    id: 1,
+    name: "마싯는 떡볶이",
+    category: "분식",
+    rating: 4.9,
+    image: ""/src/assets/1.jpg",",
+    menus: [
+      { id: 101, name: "떡볶이", price: 4500 },
+      { id: 102, name: "순대", price: 3500 },
+      { id: 103, name: "튀김 세트", price: 5000 },
+    ],
+  },
+  {
+    id: 2,
+    name: "김밥천국",
+    category: "분식",
+    rating: 4.6,
+    image: "/src/assets/2.jpg",
+    menus: [
+      { id: 201, name: "참치김밥", price: 3500 },
+      { id: 202, name: "치즈김밥", price: 4000 },
+      { id: 203, name: "라면", price: 4000 },
+    ],
+  },
+
+  {
+    id: 3,
+    name: "즉석 부대찌개",
+    category: "분식",
+    rating: 4.4,
+    image: "/src/assets/3.jpg",
+    menus: [
+      { id: 601, name: "부대찌개 1인", price: 9000 },
+      { id: 602, name: "부대찌개 2인", price: 17000 },
+      { id: 603, name: "라면 사리", price: 1000 },
+    ],
+  },
+  {
+    id: 4,
+    name: "핫도그",
+    category: "분식",
+    rating: 4.3,
+    image: "/src/assets/4.jpg",
+    menus: [
+      { id: 701, name: "치즈 핫도그", price: 2500 },
+      { id: 702, name: "감자 핫도그", price: 2500 },
+      { id: 703, name: "콤보 세트 4개", price: 9000 },
+    ],
+  },
+  {
+    id: 5,
+    name: "역전우동",
+    category: "분식",
+    rating: 4.6,
+    image: "/src/assets/5.jpg",
+    menus: [
+      { id: 801, name: "우동", price: 4500 },
+      { id: 802, name: "쫄면", price: 5000 },
+      { id: 803, name: "비빔우동", price: 5500 },
+    ],
+  },
+];
