8741023f2e859091fbe99ac84b3b01f849ca1cc8
안정규 <ahnjunggyu1114@gmail.com>
2026-05-26 19:23:33 +0900

    [FEAT] 결제 컴포넌츠 버튼 활성화

--- Week7_Homework/src/components/PaymentBox.jsx
+++ Week7_Homework/src/components/PaymentBox.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
+
+
+
 const PaymentBox = () => {
+    const [selectedMethod, setSelectedMethod] = useState("");
+
     return (
         <div className="py-[56px] px-[48px] rounded-[20px] bg-[#FFFFFF]">
             <h1 className=" pb-[60px] text-center text-[36px] font-bold">결제하기</h1>
@@ -7,10 +14,50 @@ const PaymentBox = () => {
                 <p className="text-[#F0485F] text-[20px] pb-[32px]">결제 방법</p>
 
                 <div className="grid grid-cols-2 gap-y-[24px] gap-x-[32px]">
-                    <button className="w-[220px] px-[24px] py-[12px] rounded-[5px] text-[20px] bg-[#F7F7F7]">카카오페이</button>
-                    <button className="w-[220px] px-[24px] py-[12px] rounded-[5px] text-[20px] bg-[#F7F7F7]">네이버페이</button>
-                    <button className="w-[220px] px-[24px] py-[12px] rounded-[5px] text-[20px] bg-[#F7F7F7]">카드 결제</button>
-                    <button className="w-[220px] px-[24px] py-[12px] rounded-[5px] text-[20px] bg-[#F7F7F7]">무통장 입금</button>
+
+                    <button 
+                        onClick={() => setSelectedMethod("카카오페이")}
+                        className={`cursor-pointer w-[220px] px-[24px] py-[12px] rounded-[5px] text-[20px] border ${
+                            selectedMethod === "카카오페이"
+                                ? "text-[#F0485F] border-[#F0485F] bg-[#FFDFE3]"
+                                : "text-[#111111] border-transparent bg-[#F7F7F7]"
+                        }`}
+                    >
+                        카카오페이
+                    </button>
+
+                    <button 
+                        onClick={() => setSelectedMethod("네이버페이")}
+                        className={`cursor-pointer w-[220px] px-[24px] py-[12px] rounded-[5px] text-[20px] border ${
+                            selectedMethod === "네이버페이"
+                                ? "text-[#F0485F] border-[#F0485F] bg-[#FFDFE3]"
+                                : "text-[#111111] border-transparent bg-[#F7F7F7]"
+                        }`}
+                    >
+                        네이버페이
+                    </button>
+
+                    <button 
+                        onClick={() => setSelectedMethod("카드결제")}
+                        className={`cursor-pointer w-[220px] px-[24px] py-[12px] rounded-[5px] text-[20px] border ${
+                            selectedMethod === "카드결제"
+                                ? "text-[#F0485F] border-[#F0485F] bg-[#FFDFE3]"
+                                : "text-[#111111] border-transparent bg-[#F7F7F7]"
+                        }`}
+                    >
+                        카드결제
+                    </button>
+
+                    <button 
+                        onClick={() => setSelectedMethod("무통장입금")}
+                        className={`cursor-pointer w-[220px] px-[24px] py-[12px] rounded-[5px] text-[20px] border ${
+                            selectedMethod === "무통장입금"
+                                ? "text-[#F0485F] border-[#F0485F] bg-[#FFDFE3]"
+                                : "text-[#111111] border-transparent bg-[#F7F7F7]"
+                        }`}
+                    >
+                        무통장입금
+                    </button>
                 </div>
             </div>
 
@@ -20,7 +67,15 @@ const PaymentBox = () => {
             </div>
 
             <div className="flex flex-row justify-center">
-                <button className=" rounded-[16px] px-[64px] py-[16px] text-[20px] bg-[#F7F7F7] ">00,000원 결제하기</button>
+                <button
+                    className={`cursor-pointer rounded-[16px] px-[64px] py-[16px] text-[20px] border ${
+                        selectedMethod
+                            ? "bg-[#F0485F] text-white border-[#F0485F]"
+                            : "bg-[#F7F7F7] text-[#9A9A9A] border-transparent"
+                    }`}
+                >
+                    00,000원 결제하기
+                </button>
             </div>
         </div>
     );
