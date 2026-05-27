035871865b39a1527a96eb96d5a223334086f983
LEE SANGBIN <novelsb72@naver.com>
2026-05-28 07:37:41 +0900

    [feat]: 수량 로컬스토리지 저장 기능 추가

--- Week7_Homework/src/pages/MainPage.jsx
+++ Week7_Homework/src/pages/MainPage.jsx
@@ -1,19 +1,29 @@
 import Navbar from "../components/Navbar";
 import MenuModal from "../components/MenuModal";
-import { useState } from "react";
 import RestaurantCard from "../components/RestaurantCard";
 import { restaurants } from "../data/restaurants";
+import { useState, useEffect } from "react"; 
+
 
 export default function MainPage() {
 
   const [selectedRestaurant, setSelectedRestaurant] = useState(null);
-  const [quantities, setQuantities] = useState({});
- 
+  const [quantities, setQuantities] = useState(() => {
+  // 페이지 로드할 때 로컬스토리지에서 불러오기
+  const saved = localStorage.getItem("quantities");
+  return saved ? JSON.parse(saved) : {};
+});
 
+// quantities 바뀔 때마다 로컬스토리지에 저장
+useEffect(() => {
+  localStorage.setItem("quantities", JSON.stringify(quantities));
+}, [quantities]);
+// [quantities] = quantities 바뀔 때마다 실행
+ 
   return (
     <div className="min-h-screen bg-gray-50">
       <Navbar />
-      
+
       <main className="max-w-7xl mx-auto px-4 py-8">
         <div className="grid grid-cols-1 dt:grid-cols-4 gap-4">
           {restaurants.map((restaurant) => (
