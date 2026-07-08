6b958ab6563e8bf950883cbaa899d3b5c25e037b
안정규 <ahnjunggyu1114@gmail.com>
2026-07-08 23:57:53 +0900

    week0_homework Credit 기능 구현

--- Week7_Homework/src/pages/Credit.jsx
+++ Week7_Homework/src/pages/Credit.jsx
@@ -1,6 +1,35 @@
 import Navbar from "../components/Navbar";
+import { useState } from "react";
+import useAuthStore from "../stores/useAuthStore";
 
 const Credit = () => {
+    const [selectedAmount, setSelectedAmount] = useState(0);
+
+    const user = useAuthStore((state) => state.user);
+    const setUser = useAuthStore((state) => state.setUser);
+
+    const currentCredit = user?.credit || 0;
+    const afterCredit = currentCredit + selectedAmount;
+
+    const handleCharge = () => {
+        if (!selectedAmount) {
+        alert("충전 금액을 선택해주세요.");
+        return;
+        }
+        const updatedUser = {
+        ...user,
+        credit: currentCredit + selectedAmount,
+        };
+        setUser(updatedUser);
+        const savedUsers = JSON.parse(localStorage.getItem("users")) || [];
+        const updatedUsers = savedUsers.map((savedUser) =>
+        savedUser.username === user.username ? updatedUser : savedUser
+        );
+        localStorage.setItem("users", JSON.stringify(updatedUsers));
+        alert("크레딧 충전 완료");
+        setSelectedAmount(0);
+    };
+
     return (
         <div className=" bg-[#F7F7F7]">
             <Navbar />
@@ -12,31 +41,63 @@ const Credit = () => {
                     </h1>
 
                     <div className="flex justify-center gap-[8px] mb-[32px]">
-                        <button className="w-[88px] h-[36px]" type="button">+1,000C</button>
-                        <button className="w-[88px] h-[36px]" type="button">+3,000C</button>
-                        <button className="w-[88px] h-[36px]" type="button">+5,000C</button>
-                        <button className="w-[88px] h-[36px]" type="button">+10,000C</button>
+                        <button
+                            className={`w-[88px] h-[36px] rounded-[5px] ${
+                                selectedAmount === 1000 ? "bg-[#98D2B8] text-white" : ""
+                            }`}
+                            type="button"
+                            onClick={() => setSelectedAmount(1000)}
+                        >
+                            +1,000C
+                        </button>
+                        <button
+                            className={`w-[88px] h-[36px] rounded-[5px] ${
+                                selectedAmount === 3000 ? "bg-[#98D2B8] text-white" : ""
+                            }`}
+                            type="button"
+                            onClick={() => setSelectedAmount(3000)}
+                        >
+                            +3,000C
+                        </button>
+                        <button
+                            className={`w-[88px] h-[36px] rounded-[5px] ${
+                                selectedAmount === 5000 ? "bg-[#98D2B8] text-white" : ""
+                            }`}
+                            type="button"
+                            onClick={() => setSelectedAmount(5000)}
+                        >
+                            +5,000C
+                        </button>
+                        <button
+                            className={`w-[88px] h-[36px] rounded-[5px] ${
+                                selectedAmount === 10000 ? "bg-[#98D2B8] text-white" : ""
+                            }`}
+                            type="button"
+                            onClick={() => setSelectedAmount(10000)}
+                        >
+                            +10,000C
+                        </button>
                     </div>
 
-                    <div className="mb-[56px] rounded-[8px] px-[16px] py-[18px]">
+                    <div className="mb-[56px] bg-[#F7F7F7F] rounded-[8px] px-[16px] py-[18px]">
                         <div className="flex justify-between items-center mb-[14px]">
                             <p className="">보유 크레딧</p>
-                            <p className="">5,000C</p>
+                            <p className="">{currentCredit}C</p>
                         </div>
 
                         <div className="flex justify-between items-center">
                             <p className="">충전 후 크레딧</p>
-                            <p className="">8,000C</p>
+                            <p className="">{afterCredit}C</p>
                         </div>
                     </div>
 
                     <div className="flex justify-between items-center mb-[28px]">
                         <p className="">결제금액</p>
-                        <p className="">3,000원</p>
+                        <p className="">{selectedAmount.toLocaleString()}원</p>
                     </div>
 
                     <div className="flex justify-center">
-                        <button className="w-[164px] h-[48px]" type="button">
+                        <button className="w-[164px] h-[48px] bg-[#F0485F] text-white rounded-[16px]" type="button" onClick={handleCharge}>
                             충전하기
                         </button>
                     </div>
